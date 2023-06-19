fbc1cdb05466a7c4367c8a952261ed8fc6fa1e15
Patryk <78304814+PatrykMprojects@users.noreply.github.com>
2023-06-19 14:02:18 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,77 @@
+import pickle
+import pandas as pd
+from IPython.display import display
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler
+from imblearn.over_sampling import RandomOverSampler
+import pickle
+import tabpfn
+import streamlit as st
+
+#load model and test dataset
+
+X_test = pd.read_csv('C:/Users\patry/PycharmProjects\StreamlitHeartDisease/test.csv')
+#load model
+with open('C:/Users\patry/PycharmProjects\StreamlitHeartDisease\models\modelTab.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+print(model.predict(X_test))
+
+def predict(input):
+    #change input data to an array
+    input =  [float(x) for x in input]
+    input_as_array = np.array(input)
+    #reshape the array for prediction of one patient
+    input_reshaped = input_as_array.reshape(1,-1)
+
+    prediction = model.predict(input_reshaped)
+    print(prediction)
+
+    if (prediction[0] == 0):
+        return 'Healthy patient'
+    else:
+        return 'Possible Heart Disease \n' \
+               'better check with your doctor '
+
+def main():
+
+    # Title
+    st.title(' Heart Disease Prediction ')
+    age = st.text_input('Age')
+    trestbps = st.text_input('Resting Blood Pressure')
+    chol = st.text_input('Cholesterol [mm/dl]')
+    thalach = st.text_input('Maximum Heart Rate')
+    oldpeak = st.text_input('Oldpeak, ST depression (Yes[1]/No[0])')
+    sex = st.text_input('Sex (Male[1]/Female[0])')
+    fbs = st.text_input('Fasting Blood Pressure [1: if FastingBS > 120 mg/dl, 0: otherwise]')
+    exang =  st.text_input('Exercise induced angina (Yes[1]/No[0]')
+    cp_typicalangina = st.text_input('Typical angina (Yes[1]/No[0]')
+    cp_atypical_angina = st.text_input('Atypical angina (Yes[1]/No[0]')
+    cp_non_anginalpain = st.text_input('Non anginal pain (Yes[1]/No[0]')
+    cp_asymptomatic = st.text_input('Asymptomatic (Yes[1]/No[0]')
+    restecg_normal = st.text_input('resting electrocardiogram results Normal (Yes[1]/No[0]')
+    restecg_abnormal =  st.text_input('having ST-T wave abnormality (Yes[1]/No[0]')
+    restecg_hypertrophy =  st.text_input('LVH: showing probable or definite left ventricular hypertrophy (Yes[1]/No[0]')
+    slope_up = st.text_input('slope of the peak exercise upsloping (Yes[1]/No[0]')
+    slope_flat = st.text_input('slope of the peak exercise flat (Yes[1]/No[0]')
+    slope_down = st.text_input('slope of the peak exercise downsloping (Yes[1]/No[0]')
+
+    # code for prediction
+
+    output = ''
+
+    # button
+
+    if st.button('Predict'):
+        output = predict([age, trestbps, chol, thalach, oldpeak, sex, fbs, exang, cp_typicalangina, cp_atypical_angina,
+                          cp_non_anginalpain, cp_asymptomatic, restecg_normal, restecg_abnormal, restecg_hypertrophy,
+                          slope_up, slope_flat, slope_down])
+
+    st.success(output)
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,123 @@
+import pandas as pd
+from IPython.display import display
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler
+from imblearn.over_sampling import RandomOverSampler
+import pickle
+from tabpfn import TabPFNClassifier
+from sklearn.ensemble import VotingClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import GradientBoostingClassifier, AdaBoostClassifier, BaggingClassifier, RandomForestClassifier
+
+pd.set_option('display.max_columns', None)  # Display all columns
+pd.set_option('display.max_rows', None)  # Display all rows
+pd.set_option('display.width', None)  # Set the display width to fit the entire table
+
+dataframe = pd.read_csv('../StreamlitHeartDisease/heart.csv')
+df = dataframe.copy()
+# display(df)
+
+df.rename(columns={"Age": "age", "Sex": "sex", "ChestPainType": "cp", "RestingBP": "trestbps", "Cholesterol": "chol",
+                   "FastingBS": "fbs", "RestingECG": "restecg", "MaxHR": "thalach", "ExerciseAngina": "exang",
+                   "Oldpeak": "oldpeak", "ST_Slope": "slope", "HeartDisease": "condition"}, inplace=True)
+
+df['slope'] = df['slope'].map({"Up": 0, "Flat": 1, "Down": 2})
+df['exang'] = df['exang'].map({"N": 0, "Y": 1})
+df['restecg'] = df['restecg'].map({"Normal": 0, "ST": 1, "LVH": 2})
+df['cp'] = df['cp'].map({"TA": 0, "ATA": 1, "NAP": 2, "ASY": 3})
+df['sex'] = df['sex'].map({"F": 0, "M": 1})
+
+# print(df.columns)
+df_columns = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach',
+              'exang', 'oldpeak', 'slope', 'condition']
+
+# display(test_data)
+
+df_copy = df.copy()
+
+combined_num = df[['age', 'trestbps', 'chol', 'thalach', 'oldpeak', 'sex', 'fbs', 'exang']]
+combined_cat = df[['cp', 'restecg', 'slope']]
+
+col_cat_comb = ['cp_typicalangina', 'cp_atypical_angina', 'cp_non_anginalpain', 'cp_asymptomatic', 'restecg_normal',
+                'restecg_abnormal', 'restecg_hypertrophy',
+                'slope_up', 'slope_flat', 'slope_down']
+
+encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
+
+enc_df_comb = pd.DataFrame(encoder.fit_transform(combined_cat), columns=col_cat_comb)
+
+# display(enc_df_comb)
+
+df_clean = combined_num.join(enc_df_comb)
+# Fit encoder on the training data and test data
+# display(df_copy)
+# leave 10 examples for input data
+test_data = df_clean.head(10)
+df_clean.drop(test_data.index, inplace=True)
+df_copy.drop(test_data.index, inplace=True)
+test_data.to_csv('test.csv', index=False)
+display(test_data)
+# Scaler Object
+scaler = MinMaxScaler()
+# Fit on the numeric training data
+
+
+df_clean[['age', 'trestbps', 'chol', 'thalach', 'oldpeak']] = scaler.fit_transform(
+    df_clean[['age', 'trestbps', 'chol', 'thalach', 'oldpeak']])
+
+df_clean = df_clean[df_clean.chol != 0]
+df_copy = df_copy[df_copy.chol != 0]
+
+# display(df_clean.describe())
+display(df_clean.columns)
+
+# Define random state
+rs = 42
+
+X = df_clean
+y = df_copy['condition'].astype('int')
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, stratify=y, random_state=rs)
+# print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+# ROS sampling
+ros = RandomOverSampler(random_state=rs)
+
+X_ros, y_ros = ros.fit_resample(X_train, y_train)
+# Visualize classes
+
+
+# Visualize classes
+y_ros.value_counts().plot.bar(color=['green', 'red'])
+
+plt.title('Value Counts Bar Plot')
+plt.xlabel('Values')
+plt.ylabel('Counts')
+plt.grid(True)
+
+# plt.show()
+
+
+model = TabPFNClassifier(device='cpu', seed=rs)
+
+model.fit(X_ros, y_ros)
+
+print(model.score(X_train, y_train))
+y_eval = model.predict(X_test)
+
+prediction_test = accuracy_score(y_test, y_eval)
+print('Accuracy', prediction_test)
+
+with open("models/modelTab.pkl", 'wb') as file:
+    pickle.dump(model, file)
+
+with open('models/modelTab.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+print(model.predict(X_test))
